824567c3782e3db3f11594fe7131dabd39412415
Enrique Gómez <enriquegomezbecerra213@gmail.com>
2022-03-31 12:12:29 +0200

    Calses MapReduce TextCounter

---
+++ src/MapReduce.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+class MapReduce:
+
+    def __init__(self):
+        self.map = np.array()
+
+    def map(self, file_text):
+        #Esto a lo mejor es split
+        pass
+
+    def sort(self):
+        pass
+
+    def group(self):
+        pass
+
+    def merge(self):
+        pass
+
+    def reduce(self):
+        pass
