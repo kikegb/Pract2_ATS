32f7c4bdf99b383e50672871b244bc513cc0e9b4
Enrique Gómez <enriquegomezbecerra213@gmail.com>
2022-04-28 21:08:26 +0200

    Map y sort hecho

--- src/MapReduce.py
+++ src/MapReduce.py
@@ -1,23 +1,49 @@
 import numpy as np
+from multiprocessing import Pool
 
 class MapReduce:
 
-    def __init__(self):
-        self.map = np.array()
+    def __init__(self, file_path):
+        self.file_content = self.read_file(file_path)
+        self.letter_count = {}
 
-    def map(self, file_text):
-        #Esto a lo mejor es split
+    def mapReduce(self):
+        map_result = self.map(self.file_content)
+        shuffle_result = self.shuffle(map_result)
 
-        pass
 
-    def sort(self):
-        pass
+    def map(self, file_lines):
+        p = Pool(len(file_lines))
+        result = p.map(self.sort, file_lines)
+        return result
 
-    def group(self):
-        pass
+    def sort(self, line):
+        letter_freq = {}
+        for letter in line:
+            if letter.isalpha():
+                if letter_freq.get(letter) is not None:
+                    letter_freq[letter] = letter_freq[letter] + 1
+                else:
+                    letter_freq[letter] = 1
+        return letter_freq
 
-    def merge(self):
-        pass
+    def shuffle(self, map_lines):
+        letter_freq = {}
+        for x in map_lines:
+            """Juntar diccionarios (key: letra, value: listas de 1's"""
+        return letter_freq
+
 
     def reduce(self):
+        """Calcular total frequencia letras"""
+        """Calcular porcentages"""
+        pass
+
+    def read_file(self, path):
+        f = open(path, 'r', encoding='utf-8')
+        lines = f.readlines()
+        f.close()
+        return lines
+
+    def write_file(self, path):
         pass
--- src/TextCounter.py
+++ src/TextCounter.py
@@ -1,28 +1,31 @@
 import multiprocessing as mp
 from multiprocessing import Pool
-import sys;
+import sys
+from MapReduce import MapReduce
 
 NUM_PROC = 2
-
 lista = range(100000)
 
-def bucle_chulo(lista):
-    for i in lista:
-        print("HOLA" + str(i))
-
-def main():
-    file = read_file(sys.argv[0])
-    p = Pool(3)
-    p.apply(bucle_chulo, lista)
-
-if __name__ == "__main__":
-    print("Number of cpu : ", mp.cpu_count())
-    for i in range(2):
-        main()
-    jobs = []
-
 def read_file(path):
-    f = open(path, 'r')
+    f = open(path, 'r', encoding='utf-8')
     lines = f.readlines()
     f.close()
     return lines
+
+def bucle_chulo(line):
+        letter_freq = {}
+        for letter in line:
+            if letter.isalpha():
+                if letter_freq.get(letter) is not None:
+                    letter_freq[letter] = letter_freq[letter] + 1
+                else:
+                    letter_freq[letter] = 1
+        print(letter_freq)
+
+def main(file_path):
+    mr = MapReduce(file_path)
+    mr.map()
+
+if __name__ == "__main__":
+    print("Number of cpu : ", mp.cpu_count())
+    main(sys.argv[1])
