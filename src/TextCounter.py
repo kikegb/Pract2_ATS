3d89fec0a24ea2a8a08d9f64f63664d300d5785c
Enrique Gómez <enriquegomezbecerra213@gmail.com>
2022-04-28 12:16:52 +0200

    Leer fichero medio hecho

--- src/TextCounter.py
+++ src/TextCounter.py
@@ -1,5 +1,7 @@
 import multiprocessing as mp
 from multiprocessing import Pool
+import sys;
+
 NUM_PROC = 2
 
 lista = range(100000)
@@ -9,6 +11,7 @@ def bucle_chulo(lista):
         print("HOLA" + str(i))
 
 def main():
+    file = read_file(sys.argv[0])
     p = Pool(3)
     p.apply(bucle_chulo, lista)
 
@@ -18,3 +21,8 @@ if __name__ == "__main__":
         main()
     jobs = []
 
+def read_file(path):
+    f = open(path, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
