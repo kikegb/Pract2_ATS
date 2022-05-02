6bcd1753a96309fe99ed1746377aad91061729ce
Enrique Gómez <enriquegomezbecerra213@gmail.com>
2022-05-02 20:49:33 +0200

    Funciona para un (1) fichero

--- src/MapReduce.py
+++ src/MapReduce.py
@@ -4,40 +4,62 @@ from multiprocessing import Pool
 class MapReduce:
 
     def __init__(self, file_path):
+        self.file_path = file_path
         self.file_content = self.read_file(file_path)
-        self.letter_count = {}
+        self.result = []
+        self.total_words = 0
+        self.map()
+        self.shuffle()
+        self.reduce()
+        self.calculatePercent()
+        self.printResult()
 
-    def mapReduce(self):
-        map_result = self.map(self.file_content)
-        shuffle_result = self.shuffle(map_result)
 
-
-    def map(self, file_lines):
-        p = Pool(len(file_lines))
-        result = p.map(self.sort, file_lines)
-        return result
+    def map(self):
+        p = Pool(4)
+        result = p.map(self.sort, self.file_content)
+        for x, y in result:
+            self.result.append(x)
+            self.total_words += y
 
     def sort(self, line):
+        words = line.split(" ")
         letter_freq = {}
-        for letter in line:
-            if letter.isalpha():
-                if letter_freq.get(letter) is not None:
-                    letter_freq[letter] = letter_freq[letter] + 1
-                else:
-                    letter_freq[letter] = 1
-        return letter_freq
+        total_words = 0
+        for word in words:
+            found_dict = {}
+            total_words += 1
+            for letter in word.lower():
+                if letter.isalpha():
+                    if letter not in found_dict.keys():
+                        found_dict[letter] = True
+                        if letter not in letter_freq.keys():
+                            letter_freq[letter] = [1]
+                        else:
+                            letter_freq[letter][0] += 1
+        return letter_freq, total_words
 
-    def shuffle(self, map_lines):
+    def shuffle(self):
         letter_freq = {}
-        for x in map_lines:
-            """Juntar diccionarios (key: letra, value: listas de 1's"""
-        return letter_freq
+        for x in self.result:
+            for k, v in x.items():
+                if k in letter_freq.keys():
+                    letter_freq[k].append(v[0])
+                else:
+                    letter_freq[k] = v
+        self.result = letter_freq
 
 
     def reduce(self):
-        """Calcular total frequencia letras"""
-        """Calcular porcentages"""
-        pass
+        for k, v in self.result.items():
+            letters_in_file = 0
+            for numbero in v:
+                letters_in_file += numbero
+            self.result[k] = letters_in_file
+
+    def calculatePercent(self):
+        for k, v in self.result.items():
+            self.result[k] = round((v / self.total_words) * 100, 2)
 
     def read_file(self, path):
         f = open(path, 'r', encoding='utf-8')
@@ -45,5 +67,8 @@ class MapReduce:
         f.close()
         return lines
 
-    def write_file(self, path):
-        pass
+    def printResult(self):
+        print(self.file_path + ":")
+        for k, v in self.result.items():
+            print(str(k) + " : " + str(v) + "%")
+        print("\n")
--- src/TextCounter.py
+++ src/TextCounter.py
@@ -12,19 +12,8 @@ def read_file(path):
     f.close()
     return lines
 
-def bucle_chulo(line):
-        letter_freq = {}
-        for letter in line:
-            if letter.isalpha():
-                if letter_freq.get(letter) is not None:
-                    letter_freq[letter] = letter_freq[letter] + 1
-                else:
-                    letter_freq[letter] = 1
-        print(letter_freq)
-
 def main(file_path):
     mr = MapReduce(file_path)
-    mr.map()
 
 if __name__ == "__main__":
     print("Number of cpu : ", mp.cpu_count())
